aa66aaa31e5619b09a0ea759da58ecb47a4a8787
tanht - ithoangtan <tanht@bap-ventures.com>
2022-10-04 11:54:04 +0700

    Initial: Hellix font and Home page

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const HomeStyled = styled.section`
+   display: flex;
+   flex-direction: column;
+`
+
+export default function Home() {
+  return (
+    <HomeStyled>
+      Home
+    </HomeStyled>
+  )
+}
